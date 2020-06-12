458d7cf75f5cc6f02894066608958927cadf6955
hagi <hagimizu666@gmail.com>
2020-06-12 12:53:11 +0000

    lesson13 mid kadai

---
+++ app/controllers/tasks_controller.rb
@@ -0,0 +1,54 @@
+class TasksController < ApplicationController
+  def index
+    @tasks = Task.all
+  end
+  
+  def show
+    @task = Task.find(params[:id])
+  end
+  
+  def new
+    @task = Task.new
+  end
+  
+  def create
+    @task = Task.new(task_params)
+    
+    if @task.save
+      flash[:success] = "タスクの追加に成功しました！"
+      redirect_to @task
+    else
+      flash.now[:danger] = "タスクの追加に失敗しました…"
+      render :new
+    end
+  end
+  
+  def edit
+    @task = Task.find(params[:id])
+  end
+  
+  def update
+    @task = Task.find(params[:id])
+    
+    if @task.update(task_params)
+      flash[:success] = "タスクの更新に成功しました！"
+      redirect_to @task
+    else
+      flash.now[:danger] = "タスクの更新に失敗しました…"
+      render :edit
+    end
+  end
+  
+  def destroy
+    @task = Task.find(params[:id])
+    @task.destroy
+    
+    flash[:success] = "タスクは削除されました"
+    redirect_to task_url
+  end
+  
+  def task_params
+    params.require(:task).permit(:content)
+  end
+
+end
