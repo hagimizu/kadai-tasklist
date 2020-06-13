7122177bc620474e9842a54718d3de277241d921
hagi <hagimizu666@gmail.com>
2020-06-13 09:39:18 +0000

    lesson13 mid-kadai

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -44,7 +44,7 @@ class TasksController < ApplicationController
     @task.destroy
     
     flash[:success] = "タスクは削除されました"
-    redirect_to task_url
+    redirect_to tasks_url
   end
   
   private
